e3f5e1cc45ff0736813a8dad3d59f5ae7998de68
luke <luke44@gmail.com>
2021-09-14 15:14:16 +0100

    Defined 3 functions

--- directory.rb
+++ directory.rb
@@ -11,10 +11,24 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-puts "The students of Villains Academy"
-puts "--------------"
-students.each do |student|
-  puts student
-end
+
 # End of names being entered. Summary follows
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
