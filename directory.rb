e7a483ae90e67d97be53c368334f408f93b1c99f
luke <luke44@gmail.com>
2021-09-16 13:19:42 +0100

    Setting up menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,31 @@
-def input_students
+def interactive_menu
+  students = []
+  loop do
+    # 1 Print menu and ask for user input
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2 Read input and save it to a variable
+    selection = gets.chomp
+    # 3 Do what the user asks
+    case selection
+      when "1"
+        students = input_students(students)
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+def input_students(students)
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while name is not empty, repeat this code
@@ -32,23 +55,4 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
-
-=begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
+interactive_menu
